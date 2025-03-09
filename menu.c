2474ec4660f3ca47b025c042ff1fede23ab5507b
suneater80 <hafizzulhakim142@gmail.com>
2025-03-09 21:17:25 +0700

    Menambahkan menu utama (Start, Continue, Options, Exit)

--- menu.c
+++ menu.c
@@ -1,14 +1,32 @@
-#include "raylib.h"
+#include "menu.h"
+#include <stdio.h>
 
-void ShowMenu() {
-    InitWindow(800, 600, "Main Menu");
-    
+MenuOption ShowMenu() {
+    int selectedOption = 0;
+    const char *menuOptions[] = {"Start Game", "Continue", "Options", "Exit"};
+    int totalOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Main Menu", 350, 250, 20, BLACK);
+
+        DrawText("Main Menu", 320, 100, 40, DARKGRAY);
+        for (int i = 0; i < totalOptions; i++) {
+            Color color = (i == selectedOption) ? RED : BLACK;
+            DrawText(menuOptions[i], 350, 200 + i * 50, 30, color);
+        }
+
         EndDrawing();
+
+        if (IsKeyPressed(KEY_DOWN)) {
+            selectedOption = (selectedOption + 1) % totalOptions;
+        }
+        if (IsKeyPressed(KEY_UP)) {
+            selectedOption = (selectedOption - 1 + totalOptions) % totalOptions;
+        }
+        if (IsKeyPressed(KEY_ENTER)) {
+            return (MenuOption)selectedOption;
+        }
     }
-    
-    CloseWindow();
+    return MENU_EXIT;
 }
