0b206b1b11348052ddc23028b243aaad6adc1b17
Auri Yuri <59200747+ananta-cloud@users.noreply.github.com>
2025-03-10 11:20:18 +0700

    Update (11:08 AM, 10-03-2025)
    Update sound

--- sound.c
+++ sound.c
@@ -1,26 +1,12 @@
 #include "raylib.h"
-#include <stdio.h>
 
 int main(void)
 {
-    // Initialize the audio device
     InitAudioDevice();
-    
-    // Load a sound file
-    Sound sound = LoadSound("backsound2.wav"); // Ensure the file is in the same directory
-    
-    // Start the main loop
-    while (!WindowShouldClose()) // This loop is here for illustration; no window is actually created
-    {
-        if (IsKeyPressed(KEY_SPACE)) // Play sound when SPACE key is pressed
-        {
-            PlaySound(sound);
-        }
-    }
-    
-    // Unload the sound and close the audio device
+    Sound sound = LoadSound("backsound2.wav");
+    PlaySound(sound);
+    while (!IsKeyPressed(KEY_ESCAPE)) {} // Keep the program running
     UnloadSound(sound);
     CloseAudioDevice();
-    
     return 0;
 }
