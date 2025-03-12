121a490f318c34ae5b51870b51a8f1f518869a6f
Auri Yuri <59200747+ananta-cloud@users.noreply.github.com>
2025-03-12 16:17:23 +0700

    new sound

--- sound.c
+++ sound.c
@@ -6,24 +6,18 @@ int main(void) {
     InitAudioDevice();
 
     // Create sound managers for different sound files
-    SoundManager sound1 = CreateSoundManager("backsound1.wav");
-    SoundManager sound2 = CreateSoundManager("backsound2.wav");`
+    SoundManager sound = CreateSoundManager("backsound1.wav");
+
 
     // Play the first sound
-    PlaySoundManager(&sound1);
-
-    // Wait for the user to press ESC to stop
-    while (!IsKeyPressed(KEY_ESCAPE)) {}
-
-    // Play the second sound
-    PlaySoundManager(&sound2);
+    PlaySoundManager(&sound);
 
     // Wait for the user to press ESC to stop
     while (!IsKeyPressed(KEY_ESCAPE)) {}
 
     // Unload sounds and close the audio device
-    UnloadSoundManager(&sound1);
-    UnloadSoundManager(&sound2);
+    UnloadSoundManager(&sound);
+
     CloseAudioDevice();
 
     return 0;
