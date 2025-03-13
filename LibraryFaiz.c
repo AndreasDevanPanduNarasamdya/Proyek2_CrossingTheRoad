677856e9b96a11b4c1a987f97da224417c868323
Azzam Fadlurrahman <azzam2301.af@gmail.com>
2025-03-13 15:33:19 +0700

    update level lagi

--- LibraryFaiz.c
+++ LibraryFaiz.c
@@ -102,21 +102,6 @@ void InitGame() {
 }
 
 
-void NextLevel() {
-    PermainanBerakhir = (level == 2);
-    if (!PermainanBerakhir) {
-        level++;
-        numCars += 5;
-        carSpeed = (carSpeed < 5) ? carSpeed + 1 : 5;
-        player.x = GRID_WIDTH / 2;
-        player.y = GRID_HEIGHT - 2;
-        
-        checkpoint.x = player.x;
-        checkpoint.y = player.y;
-        InitGame();
-    }
-}
-
 void ResetCombo() {
     comboMultiplier = 1;
     comboStreak = 0;
