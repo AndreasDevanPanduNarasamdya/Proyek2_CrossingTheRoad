9535fbb0f7b46bff3275c98a51e644d6595bd582
Auri Yuri <59200747+ananta-cloud@users.noreply.github.com>
2025-03-13 13:43:32 +0700

    Update sound.c

--- sound.c
+++ sound.c
@@ -1,11 +1,11 @@
-#include "sfx.h"
+#include "sound.h"
 
 // Declare a global Music variable for background music
 static Music backgroundMusic;
 
 void InitSounds() {
     InitAudioDevice(); // Initialize the audio device
-    backgroundMusic = LoadMusicStream("Assets/sound/background2.wav");
+    backgroundMusic = LoadMusicStream("Assets/sound/backsound2.wav");
     PlayMusicStream(backgroundMusic);
 }
 
