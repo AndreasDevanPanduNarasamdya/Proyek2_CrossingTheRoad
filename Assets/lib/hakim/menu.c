e541d105deedce0a119509f9cec8b611194e3858
suneater80 <hafizzulhakim142@gmail.com>
2025-03-17 09:07:46 +0700

    menambahkan options

--- Assets/lib/hakim/menu.c
+++ Assets/lib/hakim/menu.c
@@ -6,21 +6,31 @@ MenuOption ShowMenu() {
     const char *menuOptions[] = {"Start Game", "Options", "Exit"};
     int totalOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
 
+    // **Dapatkan ukuran layar**
+    int screenWidth = GetScreenWidth();
+    int screenHeight = GetScreenHeight();
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText("Main Menu", 320, 100, 40, DARKGRAY);
+        // **Judul Game di Tengah**
+        int titleWidth = MeasureText("Crossing The Road", 40);
+        DrawText("Crossing The Road", (screenWidth - titleWidth) / 2, screenHeight / 4, 40, DARKGRAY);
 
         for (int i = 0; i < totalOptions; i++) {
+            int textWidth = MeasureText(menuOptions[i], 30);
+            int posX = (screenWidth - textWidth) / 2;
+            int posY = (screenHeight / 2) + i * 50;
+
             Color textColor = (i == selectedOption) ? WHITE : BLACK;
 
             // **Kotak highlight di belakang opsi yang dipilih**
             if (i == selectedOption) {
-                DrawRectangle(340, 200 + i * 50, 160, 40, RED);
+                DrawRectangle(posX - 10, posY - 5, textWidth + 20, 40, RED);
             }
 
-            DrawText(menuOptions[i], 350, 210 + i * 50, 30, textColor);
+            DrawText(menuOptions[i], posX, posY, 30, textColor);
         }
 
         EndDrawing();
