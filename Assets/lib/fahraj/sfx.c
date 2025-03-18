70d704235993e932f07111185ba38184d8f6328b
Auri Yuri <59200747+ananta-cloud@users.noreply.github.com>
2025-03-18 16:03:00 +0700

    1

--- Assets/lib/fahraj/sfx.c
+++ Assets/lib/fahraj/sfx.c
@@ -3,7 +3,7 @@
 
 // Static Sound Variables
 static Music backgroundMusic1, menuSound;
-static Sound moveChar;
+static Sound moveChar, getHealth, getCheckpoint, pausedSound, carSound;
 
 void InitSounds()
 {
@@ -15,70 +15,83 @@ void InitSounds()
     }
 
     // Load audio files and check for errors
-    menuSound = LoadMusicStream("../../sound/backsound4.wav");
-    if (menuSound.stream.buffer == NULL)
-    {
-        TraceLog(LOG_WARNING, "Failed to load menu background sound: ../../sound/backsound4.wav");
-    }
-
-    backgroundMusic1 = LoadMusicStream("../../sound/backsound4.wav");
-    if (backgroundMusic1.stream.buffer == NULL)
-    {
-        TraceLog(LOG_WARNING, "Failed to load game background music: ../../sound/backsound4.wav");
-    }
+    menuSound = LoadMusicStream("../../sound/menu.wav");
+    backgroundMusic1 = LoadMusicStream("../../sound/background.wav");
 
     moveChar = LoadSound("../../sound/moveChar.wav");
-    if (moveChar.stream.buffer == NULL)
-    {
-        TraceLog(LOG_WARNING, "Failed to load move character sound: ../../sound/moveChar.wav");
-    }
+    getHealth = LoadSound("../../sound/getHealth.wav");
+    getCheckpoint = LoadSound("../../sound/getCheckpoint.wav");
+    pausedSound = LoadSound("../../sound/paused.wav");
+    carSound = LoadSound("../../sound/car.wav");
 
     // Set initial volume levels
-    SetMusicVolume(menuSound, 1.0f);       // Maximum volume for menu sound
-    SetMusicVolume(backgroundMusic1, 1.0f); // Maximum volume for game music
-    SetSoundVolume(moveChar, 1.0f);        // Maximum volume for move sound
+    SetMusicVolume(menuSound, 1.0f);
+    SetMusicVolume(backgroundMusic1, 1.0f);
+    SetSoundVolume(moveChar, 1.0f);
+    SetSoundVolume(getHealth, 1.0f);
+    SetSoundVolume(getCheckpoint, 1.0f);
+    SetSoundVolume(pausedSound, 1.0f);
+    SetSoundVolume(carSound, 1.0f);
 
-    // Play only one music stream at a time; do not play both simultaneously
-    PlayMusicStream(menuSound); // Start playing the menu sound by default
+    // Play the menu sound as the default background music
+    PlayMusicStream(menuSound);
 }
 
 void PlayMenuBacksound()
 {
-    // Play or update the menu background music
     if (!IsMusicStreamPlaying(menuSound))
     {
-        PlayMusicStream(menuSound); // Restart menu music if it is not playing
+        PlayMusicStream(menuSound);
     }
-    UpdateMusicStream(menuSound); // Keep the menu music updated for smooth playback
+    UpdateMusicStream(menuSound);
 }
 
 void PlayBackgroundMusic1()
 {
-    // Play or update the game background music
     if (!IsMusicStreamPlaying(backgroundMusic1))
     {
-        PlayMusicStream(backgroundMusic1); // Restart game music if it is not playing
+        PlayMusicStream(backgroundMusic1);
     }
-    UpdateMusicStream(backgroundMusic1); // Keep the game music updated for smooth playback
+    UpdateMusicStream(backgroundMusic1);
 }
 
 void PlayMoveChar()
 {
-    // Play the movement sound effect
     PlaySound(moveChar);
+}
+
+void PlayGetHealth()
+{
+    PlaySound(getHealth);
+}
+
+void PlayGetCheckpoint()
+{
+    PlaySound(getCheckpoint);
+}
+
+void PlayPausedSound()
+{
+    PlaySound(pausedSound);
+}
+
+void PlayCarSound()
+{
+    PlaySound(carSound);
 }
 
 void UnloadSounds()
 {
-    // Stop all music streams
     StopMusicStream(menuSound);
     StopMusicStream(backgroundMusic1);
 
-    // Unload all music streams and sound effects
     UnloadMusicStream(menuSound);
     UnloadMusicStream(backgroundMusic1);
     UnloadSound(moveChar);
+    UnloadSound(getHealth);
+    UnloadSound(getCheckpoint);
+    UnloadSound(pausedSound);
+    UnloadSound(carSound);
 
-    // Close the audio device to free resources
     CloseAudioDevice();
 }
--- Assets/lib/fahraj/sfx.h
+++ Assets/lib/fahraj/sfx.h
@@ -3,10 +3,15 @@
 
 #include "../raylib.h"
 
+// Function declarations for initializing, playing, and unloading sounds
 void InitSounds();
 void PlayBackgroundMusic1();
 void PlayMenuBacksound();
 void PlayMoveChar();
+void PlayGetHealth();
+void PlayGetCheckpoint();
+void PlayPausedSound();
+void PlayCarSound(); // "Mobil" in English for consistency
 void UnloadSounds();
 
 #endif
